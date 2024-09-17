1c9fb4a1ebd68f0ba1dc2b01cfe82c709d811208
Paul T Summers <psummers8@login-phoenix-rh9-2.pace.gatech.edu>
2024-09-17 17:11:58 -0400

    making some progress on PGI compilers

--- davison/linuxBuildOnly.sh
+++ davison/linuxBuildOnly.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 set -e
+
+module load nvhpc/24.5
+export PGI=/usr/local/pace-apps/manual/packages/nvhpc/24.5
+export PATH=$PGI/linux86-64/24.5/compilers/bin:$PATH
+export MANPATH=$MANPATH:$PGI/linux86-64/24.5/compilers/man
+export LM_LICENSE_FILE=$PGI/license/LICENSE.txt
+
+
+
 cd build
 echo " Cleaning up previous builds..."
 make Clean
-../../tools/genmake2 -mods ../code -optfile ../../tools/build_options/darwin_amd64_gfortran
-echo "Done compiling, moving to make depend..."
+BUILD_FILE='linux_amd64_pgf77_pace'
+echo " Building with ${BUILD_FILE}"
+../../tools/genmake2 -mods ../code -optfile ../../tools/build_options/$BUILD_FILE -rootdir ../..
+echo " Done compiling, moving to make depend..."
 make depend -s
-echo "Done with make depend, moving to make..."
+echo " Done with make depend, moving to make..."
 make -s
 cd ../run
-echo "clean up run folder, then make simlinks and run"
+echo " Clean up run folder, then make simlinks and run"
 rm *
 ln -s ../input/* .
 ln -s ../build/mitgcmuv .
--- davison/linuxBuildRun.sh
+++ davison/linuxBuildRun.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 set -e
-cd build
-echo " Cleaning up previous builds..."
-make Clean
-../../tools/genmake2 -mods ../code -optfile ../../tools/build_options/darwin_amd64_gfortran
-echo "Done compiling, moving to make depend..."
-make depend -s
-echo "Done with make depend, moving to make..."
-make -s
-cd ../run
+bash linuxRunOnly.sh
+cd run
 echo "clean up run folder, then make simlinks and run"
 rm *
 ln -s ../input/* .
