f542f4b404266deba19224f8429cae149f0299a8
Paul Summers <somonesummers@gmail.com>
2024-10-16 16:53:35 -0400

    try to track analysis figure scripts

--- analysis/visualizeAlongFlow.py
+++ analysis/visualizeAlongFlow.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-dataDir = 'shelf500/'
+dataDir = './'
 saveGif = True
 figDir = '%sfigs/' % dataDir
 dataDir = '%sresults/' % dataDir
