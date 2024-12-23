30b192f5591e6cdb052cecf9b90e82104a88873d
Paul Summers <somonesummers@gmail.com>
2024-12-23 09:45:33 -0500

    more realistic fresh water flux

--- elizaScripts/controlMelange.py
+++ elizaScripts/controlMelange.py
@@ -301,8 +301,10 @@ params01['no_slip_bottom'] = True
 params01['rigidLid'] = False
 params01['implicitFreeSurface'] = True
 params01['selectAddFluid'] = 1
-#params01['implicitViscosity'] = True
-#params01['implicitDiffusion'] = True
+params01['useRealFreshWaterFlux'] = True
+params01['exactConserv'] = True
+params01['implicitViscosity'] = True
+params01['implicitDiffusion'] = True
 params01['bottomVisc_pCell'] = True
 
 # physical parameters
