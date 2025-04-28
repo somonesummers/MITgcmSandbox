fdfb57065190e7fb685d756d702af0f7e6932c87
Paul Summers <somonesummers@gmail.com>
2025-04-28 17:41:37 -0400

    plot potential density, not absolute

--- analysis/quicklookSide.py
+++ analysis/quicklookSide.py
@@ -225,7 +225,7 @@ for k in range(len(name)):
             if(i == maxStep): #only calc pressure once
                 pressure = -1 * np.ones(salt.shape) * 1020 * 9.81 * np.repeat(np.expand_dims(z,1), salt.shape[1], axis=1) /10e3
             CT = gsw.CT_from_t(salt, data[0,:,ySlice,:], pressure)
-            density = gsw.rho(salt, CT, pressure) - 1000 #in-stu density less 1000
+            density = gsw.rho(salt, CT, 0) - 1000 #in-stu density less 1000
             densityLevels = np.linspace(25,30,26)
             cc = plt.contour(
                 np.squeeze(x[ySlice,:]),
--- analysis/quicklookSideAvg.py
+++ analysis/quicklookSideAvg.py
@@ -226,7 +226,7 @@ for k in range(len(name)):
             if(i == startStep or i == maxStep): #only calc pressure once
                 pressure = -1 * np.ones(salt.shape) * 1020 * 9.81 * np.repeat(np.expand_dims(z,1), salt.shape[1], axis=1) /10e3
             CT = gsw.CT_from_t(salt, np.average(data[0,:,1:-1,:],weights=openFrac[:,1:-1,:],axis=1), pressure)
-            density = gsw.rho(salt, CT, pressure) - 1000 #in-stu density less 1000
+            density = gsw.rho(salt, CT, 0) - 1000 #in-stu density less 1000
             densityLevels = np.linspace(25,30,26)
             cc = plt.contour(
                 np.squeeze(x[0,:]),
