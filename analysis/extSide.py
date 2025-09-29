0d12af50e66d571f251bac2c011b5b70b5d37ab0
Paul Summers <psummers@stanford.edu>
2025-09-28 20:54:34 -0400

    extendedDiag plotting options. These read the meta data and are pretty flexible to print any diag

---
+++ analysis/extSide.py
@@ -0,0 +1,306 @@
+from MITgcmutils import mds
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+import sys
+import cmocean
+import fileinput
+import gsw
+import argparse
+
+parser = argparse.ArgumentParser(description='Plot dynamics side view')
+parser.add_argument('-y','--yCrossSection', nargs=1, type=float,default = None,
+                    help='optional slice location [m]')
+parser.add_argument('-q','--quick', action='count', default=0,
+                    help='quick option for last from only, double to show plot(s)')
+parser.add_argument('-k','--kValues', nargs='*', type=int, default = None,
+                    help='option specification of views to plot [default = all]')
+parser.add_argument('-n','--numFrames', nargs='?', type=int, default = 60,
+                    help='optional specification of numFrames [default = 60]')
+parser.add_argument('-t','--timeRange', nargs=2, type=int, default = None,
+                    help='optional specification of start and endtime in DAYS [default = Full Range]')
+parser.add_argument('-s','--shadow', action='count', default=0,
+                    help='option of shadow for mélange [default (off)]')
+parser.add_argument('-xl','--xlimit', nargs='?', type=int, default = None,
+                    help='optional max x [default = entire frame]')
+parser.add_argument('-dpi','--dpi', nargs='?', type=int, default = 125,
+                    help='dpi spec [default = 125]')
+args = parser.parse_args()
+
+# Pick cross section to view from file or default
+yCrossSection = 1000
+xCrossSection = 5000
+zDepth = -50
+cleanPNGs = True
+showQuiver = False
+showZeros = True
+showDensity = True
+makeMovie = False   
+
+if(os.path.isfile('input/plotHelperLocal.py')):
+    sys.path.append('input')
+    from plotHelperLocal import *
+    print('Found experiment plotting settings')
+elif(os.path.isfile('../plotHelper.py')):
+    sys.path.append('../')
+    print('no custom plotting settings, using local default')
+    from plotHelper import *
+else:  
+    print('no defaults found')
+
+if(args.yCrossSection != None):
+    print('** Manual ySlice detected **')
+    yCrossSection = args.yCrossSection
+# if(args.yCrossSection != None):
+#     print('** Manual ySlice detected **')
+#     yCrossSection = args.yCrossSection
+# #Overwrite local settings here if desired
+# # usePcolor = True
+
+print('Plot DPI:',plotDPI,'; clean PNGs:',cleanPNGs, '; usePcolor:', usePcolor)
+
+dt = 0.0   
+for line in fileinput.input('input/data'):
+        if "deltaT=" in line:
+            dt = float(line[8:-2])
+print('dt is loaded as', dt)
+
+maxStep = 0
+sizeStep = 1e10
+startStep = 1e10
+
+for file in os.listdir('results'):
+    # print(file)
+    if "dynDiag.0" in file:
+        words = file.split(".")
+        # print(words[1])  
+        if int(words[1]) > maxStep:
+            maxStep = int(words[1])
+        if int(words[1]) < startStep and int(words[1]) > 0:
+            startStep = int(words[1])
+        if abs(int(words[1]) - startStep) < sizeStep and abs(int(words[1]) - startStep) > 0:
+            sizeStep = abs(int(words[1]) - startStep)
+
+## Custom timeshots available here
+# startStep = int(((maxStep - startStep)/startStep) * 1 / 2 )* startStep 
+# maxStep = maxStep - 10 * sizeStep
+
+if(args.timeRange != None):
+    startStep = args.timeRange[0] * 86400 / dt
+    maxStep = args.timeRange[1] * 86400 / dt
+
+if((maxStep-startStep)/sizeStep > args.numFrames):   #if more than numFrames, downscale to be less than numFrames
+    dwnScale = np.ceil(((maxStep-startStep)/sizeStep)/args.numFrames)
+    print('Reducing time resolution by', dwnScale)
+    sizeStep = sizeStep * dwnScale
+
+print('startStep,sizeStep,maxStep:',startStep,sizeStep,maxStep)
+
+#Decide if iceBerg data files exist
+if(os.path.isfile('input/bergMask.bin')):
+    isBerg = True
+    print('Found icebergs for this run')
+else:
+    isBerg = False
+
+os.system('rm -f figs/side_*.png')
+
+x = mds.rdmds("results/XC")
+y = mds.rdmds("results/YC")
+z = np.squeeze(mds.rdmds("results/RC"))
+
+ySlice = np.argmin(np.abs(y[:,0] - yCrossSection))
+print('cross section is y =', y[ySlice,0], 'index', ySlice)
+
+if(os.path.isfile('input/bathymetry.bin')):
+    topo = np.fromfile('input/bathymetry.bin', dtype='>f8')
+    topo = topo.reshape(np.shape(x))
+else:
+    topo = np.zeros(np.shape(x))
+
+if(isBerg):
+    bergMask = np.fromfile('input/bergMask.bin', dtype='>f8')
+    bergMask = bergMask.reshape(np.shape(x))
+    bergMaskNums = np.fromfile('input/bergMaskNums.bin', dtype='>f8')
+    bergMaskNums = bergMaskNums.reshape(np.shape(x))
+    bergsPerCell = np.fromfile('input/numBergsPerCell.bin', dtype='>f8')
+    bergsPerCell = bergsPerCell.reshape(np.shape(x))
+    bergContaingingCells = int(np.sum(bergMask))
+    maxDepth = np.zeros(np.shape(x))
+
+    openFrac = np.fromfile('input/openFrac.bin', dtype='>f8')
+    openFrac = openFrac.reshape((np.shape(z)[0], np.shape(x)[0], np.shape(x)[1]))
+    bergMask = np.fromfile('input/bergMask.bin', dtype='>f8')
+    bergMask = bergMask.reshape(np.shape(x))
+    for j in range(np.shape(x)[0]): #clean up non-berg parts of this mask
+            for i in range(np.shape(x)[1]):
+                if bergMask[j,i] == 0:
+                    openFrac[:,j,i] = 1
+                if topo[j,i] == 0 and i > 1:
+                    openFrac[:,j,i] = 0 #zero weight non-ocean cell
+                    
+if(np.min(openFrac) < 0 or np.max(openFrac) > 1):
+    print(f'Error in openFrac min/max: {np.min(openFrac):0.2f}/{np.max(openFrac):0.2f}')
+print('averaging over all cross sections')
+
+# dynNameList = ['heatDiag','uMomDiag','presDiag']
+dynNameList = ['presDiag']
+for dynName in dynNameList:
+    metadata = mds.parsemeta(f'results/{dynName}.{startStep:010d}.001.001.meta')
+    name = metadata['fldList']
+    # print(metadata)
+    if(args.quick > 0):
+        startStep = maxStep
+        cleanPNGs = False
+    if(args.kValues == None):
+        kList = range(len(name))
+    else:
+        kList = args.kValues
+    for k in kList:
+        print("\t" + dynName + ": " + name[k])
+        for i in np.arange(startStep, maxStep + 1, sizeStep):
+            if(showQuiver or showDensity):
+                dataQuiv = mds.rdmds("results/dynDiag", i)
+            if(isBerg):
+                dataBergs = mds.rdmds("results/BRGFlx",i)
+                if(dataBergs.shape[0] == 6):
+                    # print('ttest')
+                    openFrac[:,:,:] = dataBergs[5,:,:,:] #directly saved for these runs
+                    for jj in range(np.shape(x)[0]): #clean up non-berg parts of this mask
+                        for ii in range(np.shape(x)[1]):
+                            if bergMask[jj,ii] == 0:
+                                openFrac[:,jj,ii] = 1
+                            if topo[jj,ii] == 0 and ii > 1:
+                                openFrac[:,jj,ii] = 0 #zero weight non-ocean cell
+                    # print(np.sum(openFrac[1,:,:],axis=0))
+                    # print(np.sum(openFrac2[1,:,:],axis=0))
+                    # oldAveraging = False
+            # print(np.sum(openFrac[:,:,:],axis=(0,1)))
+            if(args.shadow > 0): #enable berg shadows here
+                if(dataBergs.shape[0] < 6):
+                    dataBergPlot = dataBergs[0,:,:,:]
+                    dataBergPlot[dataBergPlot != 0] = .2
+                else:
+                    dataBergPlot = dataBergs[5,:,:,:]
+                dataBergPlot[dataBergPlot == 0] = 1 #np.nan
+            data = mds.rdmds("results/%s"%(dynName), i)
+            kk = k
+            if(dynName == 'uMomDiag'):
+                cm = 'cmo.balance'
+                lvl = np.linspace(-2e-5,2e-5,31)
+            elif(name[k] == 'PHI_NH'):
+                cm = 'PuOr'
+                lvl = np.linspace(-1e-4,1e-4,31)
+            elif(name[k] == 'PHIHYD'):
+                cm = 'PuOr'
+                data[k,data[k,:,:,:] == 0] = np.nan
+                for tmp_i in range(data.shape[1]):
+                    data[k,tmp_i,ySlice,:] = data[k,tmp_i,ySlice,:] - np.nanmean(data[k,tmp_i,ySlice,2:-2])
+                lvl = np.linspace(-.1,.1,31)
+            else:
+                minVal = np.nanmin(data[k,:,:,:])
+                maxVal = np.nanmax(data[k,:,:,:])
+                mn = np.nanmean(data[k,:,:,:])
+                sd = np.nanstd(data[k,:,:,:])
+                if((maxVal)/(minVal) < .1 ):
+                    cm = 'cmo.balance'
+                    lvl = np.linspace(-mn-3*sd,mn+3*sd,31)
+                else:
+                    cm = 'cmo.tempo'
+                    lvl = np.linspace(mn-3*sd,mn+3*sd,31)
+            plt.figure(figsize=(12, 5))
+            if(usePcolor):
+                cp = plt.pcolormesh(
+                    np.squeeze(x[0,:]),
+                    np.squeeze(z),
+                    np.squeeze(data[kk, :, ySlice, :]),
+                    cmap=cm,
+                    vmin=np.min(lvl),
+                    vmax=np.max(lvl),
+                )
+            else:
+                cp = plt.contourf(
+                    np.squeeze(x[0,:]),
+                    np.squeeze(z),
+                    np.squeeze(data[kk, :,ySlice, :]),
+                    lvl,
+                    extend="both",
+                    cmap=cm,
+                )
+            # if(k == 5): #special bounds to highlight refreezing areas
+            #     cp.cmap.set_under('r')
+            plt.plot(x[0,:],topo[int(np.shape(x)[0]/2),:],color='black')
+            if(args.shadow > 0):
+                cp2 = plt.contourf(
+                    x[0,:],
+                    np.squeeze(z),
+                    np.squeeze(dataBergPlot[:, ySlice, :]),
+                    [.4,.6,.8,.9,.95],
+                    extend="min",
+                    alpha=.2,
+                    cmap='cmo.gray')
+                #cbar2 = plt.colorbar(cp2)
+                #cbar2.set_label('Ocean Fraction')
+            cbar = plt.colorbar(cp)
+            # cbar.set_label(cbarLabel[k])
+            if(showDensity):
+                salt = dataQuiv[1,:,ySlice,:]
+                if(i == startStep): #only calc pressure once
+                    pressure = -1 * np.ones(salt.shape) * 1020 * 9.81 * np.repeat(np.expand_dims(z,1), salt.shape[1], axis=1) /10e3
+                CT = gsw.CT_from_t(salt, dataQuiv[0,:,ySlice,:], pressure)
+                density = gsw.rho(salt, CT, 0) - 1000 #in-stu density less 1000
+                densityLevels = np.linspace(25,28,16)
+                cc = plt.contour(
+                    np.squeeze(x[0,:]),
+                    np.squeeze(z),
+                    np.squeeze(density),
+                    densityLevels,
+                    colors='black',
+                    linewidths=0.5,
+                    alpha=0.5
+                )
+                plt.clabel(cc, inline=3, fontsize=8)
+            if(showZeros):
+                if(True):
+                    cc = plt.contour(
+                        np.squeeze(x[0,:]),
+                        np.squeeze(z),
+                        np.squeeze(data[kk, :, ySlice, :]),
+                        [0],
+                        colors='gray',
+                        linewidths=0.5,
+                        alpha=0.5
+                    )
+                    plt.clabel(cc, inline=3, fontsize=8)
+            if(args.xlimit != None):
+                plt.xlim([0, args.xlimit])
+            plt.xlabel('Along Fjord [m] %.3f %.3f nan: %i' %(np.nanmin(data[kk, :, ySlice, :]),np.nanmax(data[kk, :, ySlice, :]),np.max(np.isnan(data[kk, :, ySlice, :]))))
+            plt.ylabel('Depth [m]')
+            plt.title("%s Width Averaged at %.02f days" % (name[k], i/86400.0*dt))
+            j = i/sizeStep
+            
+            str = "figs/side_%s%05i.png" % (name[k],j)
+            plt.tight_layout()
+            plt.savefig(str, format='png', dpi=args.dpi)
+            if(args.quick > 1):
+                plt.show()
+            plt.close()
+
+        if(args.quick == 0):
+            locStr = ''
+            timeStr = ''
+            if(args.yCrossSection != None):
+                locStr = f'{int(np.abs(args.yCrossSection[0]))}'
+            if(args.timeRange != None):
+                timeStr = f'_T_{args.timeRange[0]}_{args.timeRange[1]}'
+            
+            newFileName = f"figs/autoSide_{locStr}{name[k]}{timeStr}.gif"
+            os.system(f'magick -delay {500/((maxStep-startStep)/sizeStep)} figs/side_{name[k]}*.png -colors 256 -depth 256 {newFileName}')
+
+            if(makeMovie):
+                os.system('ffmpeg -r %f -i figs/side_%s%%05d.png -c:v libx264 -r 30 -pix_fmt yuv420p figs/autoSide_%s.mov' %(80/((maxStep-startStep)/sizeStep), name[k], name[k]))
+
+    #Clean up intermediate pngs
+        if(cleanPNGs):
+            os.system('rm -f figs/side_*.png')
+
