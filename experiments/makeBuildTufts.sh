185beff63fc121838dfdf24c4a723cddb8804723
Paul Thomas Summers <psumme03@login-prod-03.pax.tufts.edu>
2026-06-04 10:51:15 -0400

    adding build file for tufts

---
+++ experiments/makeBuildTufts.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+# Script for building MITgcmuv
+# Paul Summers Sept 2025
+# invoked as:
+# bash makeBuild.sh [root dir] [-mpi]
+# Root directory is assumed to be ../../..
+# -mpi builds with for mpi running. This requires specific packages to be loaded, currently this is set up for GaTech Pace
+# MPI_HOME will need to be adjusted for local configuration. 
+set -e
+
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: assuming root location of ../../.."
+  ROOT="../../.."
+elif [ "$1" == "-mpi" ]; then
+  ROOT="../../.."
+  OPT2=$1 
+else
+  ROOT=$1
+  OPT2=$2
+fi
+
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     MACHINE="Linux";;
+    Darwin*)    MACHINE="Mac";;
+esac
+echo "Idenitfied machine as ${MACHINE}"
+
+cd build
+FILE=Makefile     
+if [ -f $FILE ]; then
+   echo "File $FILE exists, cleaning"
+   make Clean
+else
+   echo "File $FILE does not exist, no cleaning needed"
+fi
+
+if [ "$MACHINE" == "Linux" ];
+then
+	#BUILD_FILE='linux_amd64_pgf77_pace'
+	module load openmpi/4.1.4 
+	#module load netcdf-fortran/4.6.1-mva2-hdf5-1.14
+	export MPI_HOME='/opt/shared/openmpi/4.1.4/'
+	#export NETCDF_HOME='/usr/local/pace-apps/spack/packages/linux-rhel9-x86_64_v3/gcc-12.3.0/netcdf-c-4.9.2-hv6rtvb7476ibpgxpi54pcgkbouzswfl/'
+	BUILD_FILE='linux_amd64_gfortran'
+else
+	BUILD_FILE='darwin_amd64_gfortran'
+fi
+
+if [ -z $OPT2 ]; then
+   echo "===== Building with ${BUILD_FILE} at $(pwd) ====="
+   $ROOT/tools/genmake2 -mods ../code -optfile $ROOT/tools/build_options/$BUILD_FILE -rootdir $ROOT
+elif [ "$2" == "-mpi" ]; then
+   echo "====== Building with MPI and ${BUILD_FILE} at $(pwd) ====="
+   $ROOT/tools/genmake2 -mods ../code -mpi -optfile $ROOT/tools/build_options/$BUILD_FILE -rootdir $ROOT
+else
+   echo "$2"
+   echo "invalid 2nd argument"
+   exit 2
+fi
+
+echo " Done compiling, moving to make depend..."
+make depend -s
+echo " Done with make depend, moving to make..."
+make -sj 4
+
+if [ "$MACHINE" == "Mac" ];
+then
+   afplay /System/Library/Sounds/Funk.aiff &
+fi 
+
+echo "(⌐■_■) Done building"
