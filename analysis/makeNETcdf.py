153dbf2499c1118692188dfce99a02fbf1ef07cf
Paul Thomas Summers <psumme03@login-prod-02.pax.tufts.edu>
2026-06-11 11:44:03 -0400

    netcdf script

--- analysis/makeNETcdf.py
+++ analysis/makeNETcdf.py
@@ -19,14 +19,16 @@ for line in fileinput.input('input/data'):
 print(f'dt loaded as {dt}')
 
 # ds = xmitgcm.open_mdsdataset('results/',ignore_unknown_vars=False, delta_t = dt)
-
-ds = xmitgcm.open_mdsdataset('results/',ignore_unknown_vars=False, delta_t = 15,iters='all')
+i_s = list(range(5760,2108160,5760))
+ds = xmitgcm.open_mdsdataset('results/',ignore_unknown_vars=False, delta_t = 15,iters=i_s,geometry='cartesian')
 
 print(ds)
 
+#this enables compression for all variables (lossless), levels are 1-9 fastest to most compressed
+encoding = {var: {"zlib": True, "complevel": 5} for var in ds.data_vars}
 # print(ds.variables)
 
-ds.to_netcdf('xarray_data.nc', encoding={"time": {"dtype": "int64"}}) #this is slow, be patient
+ds.to_netcdf('xarray_data.nc',encoding=encoding) #this is slow, be patient
 
 print('Done saving!')
 
